9793395f9deed1f44661f266c6b3cbe0770b4480
Dapinder Singh <122777321+dapinder96@users.noreply.github.com>
2025-02-22 15:06:53 +0530

    Delete backend directory

--- backend/routes/categorgyRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const categoryController = require('../controllers/categoryController');
-
-router.post('/add-multiple', categoryController.addCategories);
-router.get('/all', categoryController.getCategories);
-router.delete('/delete/:id', categoryController.deleteCategory);
-
-module.exports = router;
