a06cc91a650b2390e9b3de5f1876b499d94dcf39
arsh khaira <123055290+Khaira74@users.noreply.github.com>
2025-02-24 09:30:57 +0530

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sequelize = require('./config/database');
+const models = require('./models');
 
+// Import routes
+const countryRoutes = require('./routes/countryRoutes');
+const stateRoutes = require('./routes/stateRoutes');
+
+const categoryRoutes = require('./routes/categorgyRoutes');
+const userRoutes = require('./routes/userRoutes');
+const rolesRoutes = require('./routes/rolesRoutes');
+const departmentRoutes = require('./routes/departmentRoutes');
+const regionRoutes = require('./routes/regionRoutes');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Test DB connection
+sequelize.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.error('Error: ' + err));
+
+// Sync database with { alter: true } to prevent table recreation
+sequelize.sync({ alter: true })
+  .then(() => console.log('DB synchronized'))
+  .catch(err => console.error('Sync error: ' + err));
+
+// Example test route
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+// Using API routes
+app.use('/api/countries', countryRoutes);
+app.use('/api', stateRoutes);
+app.use('/api/departments', departmentRoutes);
+app.use('/api/category', categoryRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api', rolesRoutes);
+app.use('/api', regionRoutes);
+// Set up the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
